670f54ae11c91d2760c351aed2da3edd73778dc2
KreativeThinker <sagely2005eagle@gmail.com>
2024-09-11 22:13:48 +0530

    chore: update powerup models

--- src/pwncore/models/powerups.py
+++ src/pwncore/models/powerups.py
@@ -1,32 +1,51 @@
 from enum import Enum
 
-from tortoise import fields
-from tortoise.contrib.pydantic.creator import pydantic_model_creator
-from tortoise.models import Model
+from tortoise import fields, models
+from tortoise.contrib.pydantic import pydantic_model_creator
 
-from pwncore.models import Team
-
-__all__ = ("PowerupType", "Powerup", "Powerup_Pydantic")
+__all__ = (
+    "Powerup",
+    "PowerupType",
+    "PowerupConfig",
+    "PowerupConfig_Pydantic",
+    "TeamPowerup",
+    "Powerup_Pydantic",
+    "TeamPowerup_Pydantic",
+)
 
 
 class PowerupType(str, Enum):
     LUCKY_DRAW = "lucky_draw"
     SABOTAGE = "sabotage"
     GAMBLE = "gamble"
-    POINT_SIPHON = "point_siphon"
-    FREE_HINT = "free_hint"
+    SIPHON = "siphon"
 
 
-class Powerup(Model):
-    id = fields.IntField(pk=True)
-    type = fields.CharEnumField(PowerupType)
+class PowerupConfig(models.Model):
+    type = fields.CharEnumField(PowerupType, unique=True, null=False)
     cost = fields.IntField()
-    team: fields.ForeignKeyRelation[Team] = fields.ForeignKeyField(
-        "models.Team", related_name="powerups"
+    max_uses = fields.IntField()
+    is_active = fields.BooleanField(default=False)
+    description = fields.CharField(max_length=255)
+
+
+class Powerup(models.Model):
+    team = fields.ForeignKeyField("models.Team", related_name="powerups")
+    type = fields.CharEnumField(PowerupType)
+    used_at = fields.DatetimeField(null=True)
+    target_team = fields.ForeignKeyField(
+        "models.Team", null=True, related_name="targeted_powerups"
     )
-    used = fields.BooleanField(default=False)
-    created_at = fields.DatetimeField(auto_now_add=True)
-    max_count = fields.IntField()
+    expires_at = fields.DatetimeField(null=True)
+    profit = fields.FloatField(default=0)
 
 
+class TeamPowerup(models.Model):
+    team = fields.ForeignKeyField("models.Team", related_name="team_powerups")
+    powerup_type = fields.CharEnumField(PowerupType)
+    uses_left = fields.IntField()
+
+
+PowerupConfig_Pydantic = pydantic_model_creator(PowerupConfig)
 Powerup_Pydantic = pydantic_model_creator(Powerup)
+TeamPowerup_Pydantic = pydantic_model_creator(TeamPowerup)
